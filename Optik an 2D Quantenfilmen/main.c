faa26b2f418441bc28a1c90becb2ebbea366ad04
TheRealTurtler <michael.diedler.7399@gmail.com>
2020-07-10 18:30:32 +0200

    Added function to better calculate and save needed values

--- Optik an 2D Quantenfilmen/main.c
+++ Optik an 2D Quantenfilmen/main.c
@@ -1,37 +1,26 @@
 #include "main.h"
 
-// ================ Phys. Konstanten ======================
+// =========================== Macros ===============================
+
+#define _EV(x) ((x) * eVtoJ)
+
+// ====================== Phys. Konstanten ==========================
+
 const double planckRed = 1.054571817e-34;
 const double m_0 = 9.1093837015e-31;
 const double eVtoJ = 1.602176e-19;
 
-// ================ Main ==================================
+// ============================ Main ================================
 
 int main()
 {
-	// Konstanten
+	// Rechentoleranz
 	const double toleranz = 1e-3;
-		
-	const double m_we = 0.067 * m_0;
-	const double m_be = 0.09388 * m_0;
-	const double m_wlh = 0.087 * m_0;
-	const double m_blh = 0.10716 * m_0;
-	const double m_whh = 0.34 * m_0;
-	const double m_bhh = 0.396 * m_0;
 
-	const double V_0e = 0.259 * eVtoJ;
-
-	// Startwert für Elektronenenergie
-	double E_ne = 1e-2 * eVtoJ;
-
-	// Startwert für Lochenergie
-	double E_nh = 1e-5 * eVtoJ;
-
-	double* array;
-
-
+	// Ausgabedatei Ã¶ffnen
 	FILE* file = fopen("Optik_an_2D_Quantenfilmen.dat", "w");
 
+	// Spalten beschriften
 	fprintf(file, "%s", "E_ne [eV]\t");
 	fprintf(file, "%s", "d_e [nm]\t");
 	fprintf(file, "%s", "E_nhh [eV]\t");
@@ -39,75 +28,38 @@ int main()
 	fprintf(file, "%s", "E_nlh [eV]\t");
 	fprintf(file, "%s", "d_lh [nm]\n");
 
-	for (int i = 0; i < 100; i++)
-	{
-		// Breite für Elektronen berechnen
-		double d_e = calculateD(m_we, m_be, E_ne, V_0e);
+	// Werte berechnen und in Datei speichern
+	calcAndSave(file, _EV(1e-3), _EV(1e-2), _EV(1e-5), 9, _EV(1e-7), toleranz);
+	calcAndSave(file, _EV(1e-2), _EV(1e-1), _EV(1e-5), 90, _EV(1e-6), toleranz);
+	calcAndSave(file, _EV(0.110), _EV(0.130), _EV(1e-5), 10, _EV(1e-5), toleranz);
 
-		fprintf(file, "%lf\t", E_ne * 1 / eVtoJ);
-		fprintf(file, "%lf\t", d_e * 1 / 1e-9);
-
-		// Breite für Heavy Holes berechnen
-		array = calculateHoleEnergy(d_e, V_0e, E_nh, m_whh, m_bhh, toleranz);
-
-		fprintf(file, "%lf\t", array[0]* 1 / eVtoJ);
-		fprintf(file, "%lf\t", array[1] * 1 / 1e-9);
-
-		// Breite für Light Holes berechnen
-		array = calculateHoleEnergy(d_e, V_0e, E_nh, m_wlh, m_blh, toleranz);
-
-		fprintf(file, "%lf\t", array[0] * 1 / eVtoJ);
-		fprintf(file, "%lf\n", array[1] * 1 / 1e-9);
-
-		E_ne += 1e-3 * eVtoJ;
-	}
-	//printf("\nErgebnis:\n");
-	//printf("E_ne= %lf nm\n", E_ne * 1 / eVtoJ);
-	//printf("d_e= %lf nm\n", d_e * 1 / 1e-9);
-
-	
-
-	//printf("E_nhh= %lf eV\n", array[0] * 1 / eVtoJ);
-	//printf("d_hh= %lf nm\n", array[1] * 1 / 1e-9);
-
-	
-		
-	//printf("E_nlh= %lf eV\n", array[0] * 1 / eVtoJ);
-	//printf("d_lh= %lf nm\n", array[1] * 1 / 1e-9);
-
-	//printf("Toleranz: %.3lf %%\n", toleranz * 100);
-
-	// Tabelle
-	// E_ne | d_e | E_nhh | d_hh | E_lh | d_lh
-
+	// Datei schlieÃŸen
 	fclose(file);
 }
 
-// ================ Funktionen ============================
+// ========================= Funktionen =============================
 
+// Dicke des Potentialtops berechnen
 double calculateD(double m_w, double m_b, double E_n, double V_0)
 {
 	return ((2.0 * planckRed) / (sqrt(2.0 * m_w * E_n)) * atan(sqrt((m_w * (V_0 - E_n)) / (m_b * E_n))));
 }
 
-double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, double m_bh, double toleranz)
+// Energie der LÃ¶cher berechnen
+double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, double m_bh, double stepSizeE_nh, double toleranz)
 {
 	const double V_0h = 0.140 * eVtoJ;
 
 	static double returnArray[2];
-		
+
 	double d_h = calculateD(m_wh, m_bh, E_nh, V_0h);
 
 	double d_h_prev;
 
 	while (!(fabs(d_e - d_h) < toleranz * d_e))
 	{
-		//printf("%lf\t", E_nh * 1 / eVtoJ);
-		//printf("%lf\t", d_e * 1 / 1e-9);
-		//printf("%lf\n", d_h * 1 / 1e-9);
-
 		// Schrittweite
-		E_nh += 1e-6 * eVtoJ;
+		E_nh += stepSizeE_nh;
 
 		// Abbruchbedingung
 		if (E_nh >= V_0e)
@@ -115,10 +67,10 @@ double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, d
 			break;
 		}
 
-		// Vorherigen Wert für Breite speichern
+		// Vorherigen Wert fÃ¼r Breite speichern
 		d_h_prev = d_h;
 
-		// d für Löcher erneut berechnen
+		// d fÃ¼r LÃ¶cher erneut berechnen
 		d_h = calculateD(m_wh, m_bh, E_nh, V_0h);
 
 		// Abbruchbedingung
@@ -129,7 +81,7 @@ double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, d
 		}
 		else if ((d_h > d_e) && (d_h > d_h_prev))
 		{
-			printf("Toleranz nicht erreicht, Werte zu groß!\n");
+			printf("Toleranz nicht erreicht, Werte zu groÃŸ!\n");
 			break;
 		}
 	}
@@ -137,11 +89,44 @@ double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, d
 	returnArray[0] = E_nh;
 	returnArray[1] = d_h;
 
-	//printf("\nErgebnis:\n");
-	//printf("E_nh= %lf eV\n", E_nh * 1 / eVtoJ);
-	//printf("d_e= %lf nm\n", d_e * 1 / 1e-9);
-	//printf("d_h= %lf nm\n", d_h * 1 / 1e-9);
-	//printf("Toleranz: %.3lf %%\n", toleranz * 100);
-
 	return returnArray;
+}
+
+// Light und Heavy Hole Energie, sowie Dicke des Potentialtopfs berechnen und in Datei speichern
+void calcAndSave(FILE* file, double startE_ne, double endE_ne, double startE_nh, int stepsE_ne, double stepSizeE_nh, double toleranz)
+{
+	// Konstanten
+	const double m_we = 0.067 * m_0;
+	const double m_be = 0.09388 * m_0;
+	const double m_wlh = 0.087 * m_0;
+	const double m_blh = 0.10716 * m_0;
+	const double m_whh = 0.34 * m_0;
+	const double m_bhh = 0.396 * m_0;
+	const double V_0e = 0.259 * eVtoJ;
+
+	double E_ne = startE_ne;
+
+	double* array;
+
+	// Berechnung und Ausgabe in Datei
+	for (E_ne; E_ne < endE_ne; E_ne += (endE_ne - startE_ne) / stepsE_ne)
+	{
+		// Breite fÃ¼r Elektronen berechnen
+		double d_e = calculateD(m_we, m_be, E_ne, V_0e);
+
+		fprintf(file, "%lf\t", E_ne * 1 / eVtoJ);
+		fprintf(file, "%lf\t", d_e * 1 / 1e-9);
+
+		// Breite fÃ¼r Heavy Holes berechnen
+		array = calculateHoleEnergy(d_e, V_0e, startE_nh, m_whh, m_bhh, stepSizeE_nh, toleranz);
+
+		fprintf(file, "%lf\t", array[0] * 1 / eVtoJ);
+		fprintf(file, "%lf\t", array[1] * 1 / 1e-9);
+
+		// Breite fÃ¼r Light Holes berechnen
+		array = calculateHoleEnergy(d_e, V_0e, startE_nh, m_wlh, m_blh, stepSizeE_nh, toleranz);
+
+		fprintf(file, "%lf\t", array[0] * 1 / eVtoJ);
+		fprintf(file, "%lf\n", array[1] * 1 / 1e-9);
+	}
 }
--- Optik an 2D Quantenfilmen/main.h
+++ Optik an 2D Quantenfilmen/main.h
@@ -5,4 +5,8 @@
 
 double calculateD(double m_w, double m_b, double E_n, double V_0);
 
+double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, double m_bh, double stepSizeE_nh, double toleranz);
+
 double* calculateHoleEnergy(double d_e, double V_0e, double E_nh, double m_wh, double m_bh, double toleranz);
+
+void calcAndSave(FILE* file, double startE_ne, double endE_ne, double startE_nh, int steps, double stepSize, double toleranz);
